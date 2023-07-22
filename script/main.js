b8baa252882f135961a8629f58b55133dd3aebac
Kowalaw <140055290+Kowalaw@users.noreply.github.com>
2023-07-22 19:43:55 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,1 +1,16 @@
-lancerJeu()
+//lancerJeu()
+
+let repJoueur = document.getElementById("repJoueur")
+console.log(repJoueur)
+
+let boutonOk = document.getElementById("boutonOk")
+console.log(boutonOk)
+
+let motATaper = document.querySelector("#mot p")
+console.log(motATaper)
+
+let scoreJoueur = document.querySelector("span")
+console.log(scoreJoueur)
+
+let boutonRadio = document.querySelectorAll("#radioNiveau input")
+console.log(boutonRadio)
